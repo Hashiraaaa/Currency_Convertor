29f9f9717558d589f9df1d37ac6c264fecd01675
Hashiraaaa <hashirjancs2003@gmail.com>
2024-04-14 21:22:19 +0500

    Added Currency Convertor Application

---
+++ currency.ts
@@ -0,0 +1,47 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+//Defining the list of currencies and their exhange rates
+
+console.log(chalk.yellow("\n\t Welcome to HashTech Coding - Currency Convertor\n"));
+
+let exchangeRate: any = {
+    "USD": 1, //Base Currency (Dollar)
+    "EUR": 0.9, //Europeon Currency (Euro)
+    "JYP": 113, //Japanese Currrency (Yen)
+    "CAD": 1.37, //Canadian Dollar
+    "AUD": 1.65, //Australian Dollar
+    "PKR": 277.70 //Pakistani Rupee
+}
+
+let userAns = await inquirer.prompt([
+    {
+        name : "from_currency",
+        type : "list",
+        message: "Select the currency to convert from: ",
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR"]
+    },
+    {
+        name : "to_currency",
+        type : "list",
+        message: "Select the currency you want to convert into: ",
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR"] 
+    },
+    {
+        name: "amount",
+        type: "input",
+        message: "Enter the amount too convert: ",
+    }
+]);
+
+//Converting the currency formula
+
+let from_amount = exchangeRate[userAns.from_currency];
+let to_amount = exchangeRate[userAns.to_currency];
+let amount = userAns.amount;
+
+let base_amout = amount / from_amount;
+let converted_amount = base_amout * to_amount;
+
+console.log(`Converted Amount = ${chalk.green(converted_amount.toFixed(2))}`);
